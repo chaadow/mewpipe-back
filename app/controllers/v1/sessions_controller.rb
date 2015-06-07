68faa737064099c5a24bc3a5d733be0724bb99b3
chaadow <chaadow@msn.com>
2015-06-08 00:46:52 +0200

    Fix serializer

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -9,7 +9,7 @@ class V1::SessionsController < V1::BaseController
 
       if @user.valid_password?(params[:password])
         sign_in :user, @user
-        render json: @user, serializer: SessionSerializer, root: nil
+        render json: @user, serializer: V1::SessionSerializer, root: nil
       else
         invalid_login_attempt
       end
