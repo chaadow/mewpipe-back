0192c7fb5ada6cc9fc5a72e8b80a81616acbc083
chaadow <chaadow@msn.com>
2015-06-08 23:51:51 +0200

    add tags

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,13 +3,16 @@ class Video < ActiveRecord::Base
   # Scopes
   scope :recent, -> { order('created_at DESC') }
 
+  acts_as_taggable
+
   # Associations
   belongs_to :user
 
   has_attached_file :file, :styles => {
     :mp4 => { :geometry => "400x300", :format => 'mp4'},
 
-    :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }
+    :thumb => { :geometry => "300x300#", :format => 'jpg', :time => 10 },
+    :poster => { :geometry => "1000>", :format => 'jpg', :time => 10}
   }, :processors => [:transcoder]
 
   # Validations
