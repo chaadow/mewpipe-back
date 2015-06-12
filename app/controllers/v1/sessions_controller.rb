f1e3eecedc4f65f937ad0903e69cb7f3d83a0fb0
chaadow <chaadow@msn.com>
2015-06-12 03:08:58 +0200

    Add openid genom

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,14 +4,29 @@ class V1::SessionsController < V1::BaseController
 
     # POST /v1/login
     def create
-      @user = User.find_for_database_authentication(email: params[:email])
-      return invalid_login_attempt unless @user
-
-      if @user.valid_password?(params[:password])
-        sign_in :user, @user
-        render json: @user, serializer: V1::SessionSerializer, root: "session"
+      if params[:identity_url]
+        authenticate_with_open_id(params[:identity_url]) do |result, identity_url|
+          if result.successful?
+            # FIXME - needs normalizing before
+            # checking for the identity_url
+            unless user = User.find_by(identity_url: identity_url)
+              user = User.create(identity_url: identity_url)
+            end
+            sign_in user
+          else
+            invalid_login_attempt
+          end
+        end
       else
-        invalid_login_attempt
+        @user = User.find_for_database_authentication(email: params[:email])
+        return invalid_login_attempt unless @user
+
+        if @user.valid_password?(params[:password])
+          sign_in :user, @user
+          render json: @user, serializer: V1::SessionSerializer, root: "session"
+        else
+          invalid_login_attempt
+        end
       end
     end
 
