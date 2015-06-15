eff6b3e2904700831953542bd6ae33c294d29932
chaadow <chaadow@msn.com>
2015-06-15 14:32:12 +0200

    set api domain

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :user, only: []
 
-  constraints :subdomain => /^api(\.|$)/ do
+  constraints :subdomain => "api" do
     api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.mewpipe.com; version=1"}, :path => {:value => "v1"}, :defaults => {:format => "json"}, :default => true) do
     resources :users
     resources :videos do
